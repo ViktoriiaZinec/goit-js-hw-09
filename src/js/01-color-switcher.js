856498bd6aa7e577a073ca80d88d56baecd7e580
ViktoriiaZinec <victoria.zinec@gmail.com>
2023-05-05 00:27:47 +0300

    Add color-switcher.js

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,30 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.body;
+console.log(startBtn);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+let timerId = null;
+
+document.onclick = function (e) {
+  if (e.target === startBtn) {
+    startBtn.setAttribute('disabled', 'true');
+  } else startBtn.removeAttribute('disabled');
+};
+
+startBtn.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+
+    console.log('sec');
+  }, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+});
